b083cab1b50be27c104b454cc9fe2cad7d1398d2
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-08 10:24:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!
-    </div>
-  );
+import {Component} from "react";
+import { Searchbar} from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Loader } from './';
+// import { Button } from './';
+import { Modal } from './Modal/Modal';
+
+import styles  from './App.module.css';
+
+
+export class App  extends Component { 
+  state = {
+    images:[],
+    isOpen: false,
+  };
+
+  handleSubmit(){
+    console.log(5);
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  };
+
+  render(){
+    const { isOpen } = this.state;
+    return (
+      <div className={styles.App}> 
+        <Searchbar onSubmit={this.handleSubmit}/>
+        <ImageGallery images={this.state.images}/>
+        <button
+              onClick={this.toggle}              
+              type="button"
+            >  Call to action
+            </button>     
+        {/* <Loader/> */}
+        {/* <Button onClick={this.toggle}/>  */}
+        {isOpen && (
+        <Modal onClose={this.toggle}/>  )}     
+      </div> 
+    );
+  }  
 };
