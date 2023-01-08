e92d2874e586ac7afad24730c6dab924c7be5834
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-08 10:24:43 +0200

    Create ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import { ImageGalleryItem } from './ImageGalleryItem'
+import styles  from '../ImageGallery/ImageGallery.module.css';
+
+
+
+
+export const ImageGallery = ({images}) => {
+   return (  
+        <ul className={styles.ImageGallery}>              
+            {images.map(key=>( 
+                <ImageGalleryItem key={key}/>
+            ))}   
+       </ul>         
+    );
+}
+    
+  
+// ImageGallery.propTypes = {     
+//     images: PropTypes.arrayOf(),
+// }
