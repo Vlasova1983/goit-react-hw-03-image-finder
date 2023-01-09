7228c601f09ea4ce8fc57c067e9a4e09dc6e6a07
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-09 17:48:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,67 @@
 import {Component} from "react";
 import { Searchbar} from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-// import { Loader } from './';
-// import { Button } from './';
-import { Modal } from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+import { Button } from './Button/Button';
+
 
 import styles  from './App.module.css';
-
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
 export class App  extends Component { 
   state = {
-    images:[],
-    isOpen: false,
+    value:'',
+    images: null,    
+    isLoading: false, 
+    isLoadMore: false,    
   };
 
-  handleSubmit(){
-    console.log(5);
+    Page=1;
+
+  // componentDidMount(){    
+  //   console.log(this.state);    
+  // }
+
+  // componentDidUpdate(){
+  //   console.log(this.state.page);
+  //   this.setState({page:this.state.page+1});
+  // } 
+
+  fetchData = async ({value, page}) => {    
+    try {
+      const response= await axios.get(`https://pixabay.com/api/?key=31294159-be9d27b57dbd5b4db758a00af&q=${value}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`);
+      const images = response.data.hits; 
+      this.setState({images:images});                  
+    }
+    catch (error) {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+    finally{
+      this.setState({isLoading:false});
+      
+    }       
   }
+    
 
-  toggle = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  handleSubmit = async (value)=>{       
+    this.setState({isLoading:true, value: value, isLoadMore:true});       
+    this.fetchData(value,1);
   };
 
+  onClickLoadMore = () => {
+    const {value}=this.state;
+    this.fetchData(value, 2);
+  }
+  
   render(){
-    const { isOpen } = this.state;
+    const { images, isLoading, isLoadMore} = this.state;
     return (
       <div className={styles.App}> 
-        <Searchbar onSubmit={this.handleSubmit}/>
-        <ImageGallery images={this.state.images}/>
-        <button
-              onClick={this.toggle}              
-              type="button"
-            >  Modal
-            </button>     
-        {/* <Loader/> */}
-        {/* <Button onClick={this.toggle}/>  */}
-        {isOpen && (
-        <Modal onClose={this.toggle}/>  )}     
+        <Searchbar  onSubmit={this.handleSubmit}/>
+        <ImageGallery images={images}/>        
+        {isLoading && <Loader/>}
+        {isLoadMore && <Button onClick={this.onClickLoadMore} />}            
       </div> 
     );
   }  
