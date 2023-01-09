575c6778495e48d30fed2c6b803276631d407f66
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-09 17:48:29 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,21 +1,21 @@
 import PropTypes from "prop-types";
+
+
 import { ImageGalleryItem } from './ImageGalleryItem'
 import styles  from '../ImageGallery/ImageGallery.module.css';
-
-
 
 
 export const ImageGallery = ({images}) => {
    return (  
         <ul className={styles.ImageGallery}>              
-            {images.map(key=>( 
-                <ImageGalleryItem key={key}/>
+            {images?.map((image)=>( 
+                <ImageGalleryItem key={image.id} image={image}/> 
             ))}   
        </ul>         
     );
 }
     
   
-// ImageGallery.propTypes = {     
-//     images: PropTypes.arrayOf(),
-// }
+ImageGallery.propTypes = {     
+    images: PropTypes.arrayOf(PropTypes.shape()),   
+}
