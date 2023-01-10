52d696b97912749869681a1c3c8d15d2c16f2b78
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-10 21:16:03 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,35 +1,23 @@
 
-import {Component} from "react";
+import { PropTypes } from 'prop-types';
 
 
 import { ImageGalleryItem } from './ImageGalleryItem'
 import styles  from '../ImageGallery/ImageGallery.module.css';
 
 
-export class ImageGallery extends Component {
-
-
-    // shouldComponentUpdate(nextProps){
-    //   const listId  = this.props.images?.map((image)=>(image.id));        
-    //     const listIdNow = nextProps.images?.map((image)=>(image.id));
-        
-    //         console.log(listId);
-    //         console.log(listIdNow);
-    //         return true;
-        
-        
-    // }
-
-    render(){
-        return (  
-            <ul className={styles.ImageGallery}>              
-                {this.props.images?.map((image)=>( 
-                    <ImageGalleryItem key={image.id} image={image}/> 
-                ))}   
-            </ul>         
-        );
-    }  
+export const ImageGallery = ({images}) => {  
+    return (  
+        <ul className={styles.ImageGallery}>              
+            {images?.map((image)=>( 
+                <ImageGalleryItem key={image.id} image={image}/> 
+            ))}   
+        </ul>         
+    );
+}  
    
+ImageGallery.propTypes = { 
+    images: PropTypes.arrayOf( PropTypes.shape()), 
 }
     
   
